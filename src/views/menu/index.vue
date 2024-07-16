89b48cbc0d61bf1940be20e7697925079925f835
黑虎阿福 <hsf6661@163.com>
2024-07-16 22:21:57 +0800

    🐭新增二级菜单

---
+++ src/views/menu/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="menu-box">
+    <fu-menu :data="data1" background-color="red" active-text-color="white" default-active="1"></fu-menu>
+  </div>
+</template>
+
+<script setup lang="ts">
+let data1 = [
+  {
+    name: '导航1',
+    index: '1',
+    icon: 'document'
+  },
+  {
+    name: '导航2',
+    index: '2',
+    icon: 'document'
+  },
+  {
+    name: '导航3',
+    index: '3',
+    icon: 'document',
+    children: [
+      {
+        name: '导航3-1',
+        index: '3-1',
+        icon: 'document'
+      }
+    ]
+  }
+]
+</script>
+
+<style scoped lang="scss">
+.menu-box {
+  width: 200px;
+}
+</style>
