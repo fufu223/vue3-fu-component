154fc66bb27f5312133413c14191a96dff59e686
黑虎阿福 <hsf6661@163.com>
2024-07-21 23:00:27 +0800

    🐍feat：新增多级菜单

--- src/components/menu/index.ts
+++ src/components/menu/index.ts
@@ -1,10 +1,14 @@
 import { App } from 'vue'
 // @ts-ignore
+// 两层菜单
 import menu from './src/index.vue'
+// 无限层级菜单
+import infiniteMenu from './src/menu'
 
 // 让这个组件可以通过use的方式使用
 export default {
     install(app:App) {
         app.component('fu-menu', menu)
+        app.component('fu-infinite-menu', infiniteMenu)
     }
 }
--- src/components/menu/src/type.ts
+++ src/components/menu/src/type.ts
@@ -1,6 +1,8 @@
 export interface MenuItem {
     // 导航图标
     icon?: string,
+    // jsx图标(因为jsx里无法像js里直接用toLine做图标名称拼接)
+    jsxIcon?: any,
     // 导航名称
     name: string,
     // 导航标识
